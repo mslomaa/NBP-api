c01e390c8187c2e85f63b5e881d2bbcabd83687f
mslomaa <slomkowskimateusz@gmail.com>
2021-11-15 11:26:24 +0100

    controller and routing configuration

--- app/Http/Controllers/CurrencyController.php
+++ app/Http/Controllers/CurrencyController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class CurrencyController extends Controller
 {
-    //
+    public function index()
+    {
+        return view("currency");
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CurrencyController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// Route::get('/currency', CurrencyController::class, 'index');
+Route::get('/currency', 'CurrencyController@index');
