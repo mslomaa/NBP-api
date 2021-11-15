114e6474192d0fec1c9e3b72ebe965ffae547780
mslomaa <slomkowskimateusz@gmail.com>
2021-11-15 16:08:43 +0100

    api configuration

--- app/Http/Controllers/CurrencyController.php
+++ app/Http/Controllers/CurrencyController.php
@@ -3,11 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use app\Models\Currency;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\DB;
 
 class CurrencyController extends Controller
 {
     public function index()
     {
+        $currencies = Http::get('http://api.nbp.pl/api/exchangerates/tables/a/')->json();
+
+        for ($i = 0; $i < sizeof($currencies[0]['rates']); $i++){
+            $currencyCode = $currencies[0]['rates'][$i]['code'];
+            $currencysName = $currencies[0]['rates'][$i]['currency'];
+            $currencyRate = $currencies[0]['rates'][$i]['mid'];
+            
+            if ($currencyRate == null){
+                DB::table('currency')
+                    ->update([
+                        'name' => $currencysName,
+                        'currency_code' => $currencyCode,
+                        'exchange_rate' => $currencyRate
+                    ]);
+
+            } else {
+                DB::table('currency')
+                    ->insert([
+                        'name' => $currencysName,
+                        'currency_code' => $currencyCode,
+                        'exchange_rate' => $currencyRate
+                    ]);
+            }
+        }
+
+        
         return view("currency");
     }
 }
