4b46a199c9719f685a16fa256f96f2588379583a
mslomaa <slomkowskimateusz@gmail.com>
2021-11-16 11:26:19 +0100

    configuration update or insert

--- app/Http/Controllers/CurrencyController.php
+++ app/Http/Controllers/CurrencyController.php
@@ -11,32 +11,21 @@ class CurrencyController extends Controller
 {
     public function index()
     {
-        $currencies = Http::get('http://api.nbp.pl/api/exchangerates/tables/a/')->json();
+        $currencies = Http::get('http://api.nbp.pl/api/exchangerates/tables/a')->json();
 
         for ($i = 0; $i < sizeof($currencies[0]['rates']); $i++){
             $currencyCode = $currencies[0]['rates'][$i]['code'];
             $currencysName = $currencies[0]['rates'][$i]['currency'];
             $currencyRate = $currencies[0]['rates'][$i]['mid'];
-            
-            if ($currencyRate == null){
-                DB::table('currency')
-                    ->update([
-                        'name' => $currencysName,
-                        'currency_code' => $currencyCode,
-                        'exchange_rate' => $currencyRate
-                    ]);
 
-            } else {
-                DB::table('currency')
-                    ->insert([
-                        'name' => $currencysName,
-                        'currency_code' => $currencyCode,
-                        'exchange_rate' => $currencyRate
-                    ]);
-            }
+
+            DB::table('currency')
+                ->updateOrInsert(
+                        ['name' => $currencysName, 'currency_code' => $currencyCode],
+                        ['exchange_rate' => $currencyRate]
+                );
         }
 
-        
         return view("currency");
     }
 }
